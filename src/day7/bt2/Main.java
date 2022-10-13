89ace9cb1b5a16ee3dd1ce355f5f92dcfd9cb8ce
Nguyen Dong Nam <dongnam.japan@gmail.com>
2022-10-13 14:55:56 +0700

    Lớp Point2D và lớp Point3D

---
+++ src/day7/bt2/Main.java
@@ -0,0 +1,11 @@
+package day7.bt2;
+
+public class Main {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(10f, 5f);
+        System.out.println(point2D);
+
+        Point3D point3D = new Point3D(10f, 5f, 3f);
+        System.out.println(point3D);
+    }
+}
