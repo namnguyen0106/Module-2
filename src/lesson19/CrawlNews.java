e1746751fe868c5d497550e939a1af38c68bb37b
Nguyen Dong Nam <dongnam.japan@gmail.com>
2022-10-27 09:06:25 +0700

    crawl data from dantri.com.vn

---
+++ src/lesson19/CrawlNews.java
@@ -0,0 +1,35 @@
+package lesson19;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CrawlNews {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://dantri.com.vn/the-gioi.htm");
+            Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+            scanner.useDelimiter("\\Z");
+            String content = scanner.next();
+            scanner.close();
+            content = content.replaceAll("\\n+", "");
+            Pattern p = Pattern.compile("article-title\">(.*?)</a>");
+            Matcher m = p.matcher(content);
+            while (m.find()) {
+                String newStr = m.group(1).replaceAll("<a(.*?)>", "");
+                System.out.println(newStr);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
